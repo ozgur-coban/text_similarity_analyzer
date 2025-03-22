ecf5317f82d2793174aaa6753fe9b7028a4fcf24
Özgür ÇOBAN <ozgurcoban213@gmail.com>
2025-03-22 18:13:39 +0300

    feat: Implement n-gram and BOW

--- src/similarity_calculation/text_similarity.py
+++ src/similarity_calculation/text_similarity.py
@@ -1,5 +1,6 @@
 import json
-from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
@@ -18,25 +19,38 @@ class TextSimilarity:
             data = json.load(file)
         return data
 
-    def calculate_similarity(self):
+    def calculate_similarity_tdidf(self, n=1):
         """Calculate similarity percentage using TF-IDF and Cosine Similarity."""
         # Initialize the TF-IDF Vectorizer
-        vectorizer = TfidfVectorizer(stop_words="english")
+        vectorizer = TfidfVectorizer(stop_words="english", ngram_range=(n, n))
 
         # Transform the texts into TF-IDF vectors
         tfidf_matrix = vectorizer.fit_transform(
             [self.first_file_text, self.second_file_text]
         )
-
-        # Calculate cosine similarity between the two texts
+        # vocabulary = vectorizer.get_feature_names_out()
+        # print(vocabulary)
+        # df_tfidf = pd.DataFrame(tfidf_matrix.toarray(), columns=vocabulary)
+        # df_tfidf.to_csv("../data/outputs/tfidf_output.csv", index=False)
         similarity_matrix = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
-
-        # Get similarity percentage (cosine similarity score between 0 and 1)
         similarity_percentage = similarity_matrix[0][0] * 100
 
         return similarity_percentage
 
+    def calculate_similarity_bow(self, n=1):
+        vectorizer = CountVectorizer(stop_words="english", ngram_range=(n, n))
+        bow_matrix = vectorizer.fit_transform(
+            [self.first_file_text, self.second_file_text]
+        )
+        # vocabulary = vectorizer.get_feature_names_out()
+        # df_bow = pd.DataFrame(bow_matrix.toarray(), columns=vocabulary)
+        # df_bow.to_csv("../data/outputs/bow_output.csv", index=False)
+        similarity_matrix = cosine_similarity(bow_matrix[0:1], bow_matrix[1:2])
+        similarity_percentage = similarity_matrix[0][0] * 100
+        return similarity_percentage
+
     def display_similarity(self):
         """Display the calculated similarity."""
-        similarity_percentage = self.calculate_similarity()
+        similarity_percentage = self.calculate_similarity_tdidf()
+        # similarity_percentage = self.calculate_similarity_bow()
         print(f"Text1, Text2: similarity percentage = {similarity_percentage:.2f}%")
